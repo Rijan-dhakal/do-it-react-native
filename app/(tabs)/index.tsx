faa660c469c28e690a4b34cf4250b8803479084a
Rijan Dhakal <dhakalrijan01@gmail.com>
2025-10-17 22:11:32 +0545

    Complete tabs navigation between two tabs(todos and settings).

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>
+        Edit app/index.tsx to edit this screen1111.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#030303",
+  },
+
+  content: {
+    color: "white",
+  },
+});
