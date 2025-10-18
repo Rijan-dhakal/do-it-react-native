37d62853101292eef555c2411e03200e2b4afc38
Rijan Dhakal <dhakalrijan01@gmail.com>
2025-10-18 12:48:55 +0545

    Implement dark/light mode toggle with persistent state in AsyncStorage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+// import {toggleDarkMode} from "@"
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
         Edit app/index.tsx to edit this screen1111.
       </Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,7 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#030303",
   },
 
   content: {
